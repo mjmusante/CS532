a36237e3e50be647893fa7eaf2cad46185dc7d43
mjusante <mmusante@bu.edu>
2015-09-29 07:05:16 -0400

    trivial test

---
+++ hw2/test.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python
+
+from __future__ import print_function
+
+import sys
+import unittest
+
+class BresenhamTest(unittest.TestCase):
+    def test_create_object(self):
+        foo = hw2.window(300, 300)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
