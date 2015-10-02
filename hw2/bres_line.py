c1a60e9681d887918e73335ebc0a6776137c3b27
mjusante <mmusante@bu.edu>
2015-10-01 20:33:58 -0400

    Finished testing Bresenham algorithm

--- hw2/bres_line.py
+++ hw2/bres_line.py
@@ -26,11 +26,27 @@ def plotPixel(x, y):
     glEnd()
 
 
+#
+# This function uses Bresenham's algorithm for drawing lines. There
+# are three cases to deal with:
+#
+#   1.  A vertical line (dx == 0). This is a special case where
+#       we just draw a straight line from the start to the end.
+#
+#   2.  The line is more vertical than horizintal (|dy| > |dx|).
+#       For this case, we flip Breshanham's algorithm on its side
+#       and walk from y1 to y2, filling in all the x coordinates
+#       along the way.
+#
+#   3.  The line is more horizontal than vertical (|dx| > |dy|).
+#       For this case, we just use the standard algorithm,
+#       walking the x-axis from x1 to x2 and fill in the y
+#       coordinates along the way.
+#
 def draw_bres_line(p1x, p1y, p2x, p2y):
     dx = p2x - p1x
     dy = p2y - p1y
     if dx == 0:
-        # special-case code for vertical lines
         if dy > 0:
             y = p1y
             end = p2y
@@ -42,41 +58,102 @@ def draw_bres_line(p1x, p1y, p2x, p2y):
             y += 1
         return
 
-    k = math.fabs(dy / dx)
-    p = 2 * (dy - dx)
+    if abs(dy) > abs(dx):
 
-    c1 = 2 * dy
-    c2 = 2 * (dy - dx)
+        # flip the coordinates if needed
+        if p1y > p2y:
+            x = p2x
+            p2x = p1x
+            p1x = x
+            y = p2y
+            p2y = p1y
+            p1y = y
+            dx = p2x - p1x
+            dy = p2y - p1y
+        else:
+            x = p1x
+            y = p1y
 
-    if p1x > p2x:
-        x = p2x
-        y = p2y
-        end = p1x
-    else:
-        x = p1x
-        y = p1y
-        end = p2x
+        # calculate the end points and the p, c1, and c2 values
+        endx = p2x
+        endy = p2y
+        dx = abs(dx)
+        p = 2 * dx - dy
+        c1 = 2 * dx
+        c2 = 2 * (dx - dy)
 
-    err = 0.0
-    while x < end:
+        # Are we increasing or decreasing x?
+        delta = 1
+        if x > endx:
+            delta = -1
+
+        # loop over the y axis and plot points along the x axis
         plotPixel(x, y)
-        err += k
-        while err >= 0.5:
+        while y < endy:
+            y += 1
+            if p < 0:
+                p += c1
+            else:
+                p += c2
+                x += delta
             plotPixel(x, y)
-            if dy < 0:
-                y -= 1
-            elif dy > 0:
-                y += 1
-            err -= 1.0
-        x += 1
+
+    else:
+
+        # flip the coordinates if needed
+        if p1x > p2x:
+            x = p2x
+            p2x = p1x
+            p1x = x
+            y = p2y
+            p2y = p1y
+            p1y = y
+            dx = p2x - p1x
+            dy = p2y - p1y
+        else:
+            x = p1x
+            y = p1y
+
+        # calculate the end points and the p, c1, and c2 values
+        endx = p2x
+        endy = p2y
+        dy = abs(dy)
+        p = 2 * dy - dx
+        c1 = 2 * dy
+        c2 = 2 * (dy - dx)
+
+        # Are we increasing or decreasing y?
+        delta = 1
+        if y > endy:
+            delta = -1
+
+        # loop over the x axis and plot points along the y axis
+        plotPixel(x, y)
+        while x < endx:
+            x += 1
+            if p < 0:
+                p += c1
+            else:
+                p += c2
+                y += delta
+            plotPixel(x, y)
 
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-    glColor3f(0.4, 1.0, 0.0)
 
+    # draw the lines
+    glColor3f(0.0, 0.4, 1.0)
     for i in point_list:
         draw_bres_line(*i)
+
+
+    # draw the points of each line so we can see visually
+    # in the window if the lines are connecting the dots
+    glColor3f(0.4, 1.0, 0.0)
+    for i in point_list:
+        plotPixel(i[0], i[1])
+        plotPixel(i[2], i[3])
 
     glutSwapBuffers()
 
@@ -97,13 +174,6 @@ def main():
     glutCreateWindow("GLUT Window")
     glutDisplayFunc(display)
     glutReshapeFunc(reshape)
-
-    for i in point_list:
-        (x1, y1, x2, y2) = i
-        if x2 - x1 == 0:
-            print("Slope %s: infinite" % (i,))
-        else:
-            print("Slope %s: %.2f" % (i, (y2 - y1) / (x2 - x1)))
 
     glClearColor(0.2, 0.2, 0.2, 1.0)
 
